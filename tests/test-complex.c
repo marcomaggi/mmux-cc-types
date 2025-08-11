8e1ce8f9157df7d3fe39a821ba0cb5a32c34172a
Marco Maggi <mrc.mgg@gmail.com>
2025-08-11 10:26:37 +0200

    complex tests

--- tests/test-complex.c
+++ tests/test-complex.c
@@ -386,6 +386,179 @@ test_complex_conjugate (void)
 
 
 /** --------------------------------------------------------------------
+ ** Complex argument.
+ ** ----------------------------------------------------------------- */
+
+static void
+test_complex_arg (void)
+{
+  printf("running test: %s\n", __func__);
+
+#undef  REP
+#define REP	0.123
+#undef  IMP
+#define IMP	0.456
+#undef  ARG
+#define ARG	1.307'329'79
+#undef  EPS
+#define EPS	1e-6
+
+  if (1) {
+    auto Z = mmux_complexf_make_rectangular(mmux_float_make(REP), mmux_float_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+
+  if (1) {
+    auto Z = mmux_complexd_make_rectangular(mmux_double_make(REP), mmux_double_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXLD
+  if (1) {
+    auto Z = mmux_complexld_make_rectangular(mmux_ldouble_make(REP), mmux_ldouble_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXF32
+  if (1) {
+    auto Z = mmux_complexf32_make_rectangular(mmux_float32_make(REP), mmux_float32_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXF64
+  if (1) {
+    auto Z = mmux_complexf64_make_rectangular(mmux_float64_make(REP), mmux_float64_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXF128
+  if (1) {
+    auto Z = mmux_complexf128_make_rectangular(mmux_float128_make(REP), mmux_float128_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXF32X
+  if (1) {
+    auto Z = mmux_complexf32x_make_rectangular(mmux_float32x_make(REP), mmux_float32x_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXF64X
+  if (1) {
+    auto Z = mmux_complexf64x_make_rectangular(mmux_float64x_make(REP), mmux_float64x_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXF128X
+  if (1) {
+    auto Z = mmux_complexf128x_make_rectangular(mmux_float128x_make(REP), mmux_float128x_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXD32
+  if (1) {
+    auto Z = mmux_complexd32_make_rectangular(mmux_decimal32_make(REP), mmux_decimal32_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXD64
+  if (1) {
+    auto Z = mmux_complexd64_make_rectangular(mmux_decimal64_make(REP), mmux_decimal64_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+#ifdef MMUX_CC_TYPES_HAS_COMPLEXD128
+  if (1) {
+    auto Z = mmux_complexd128_make_rectangular(mmux_decimal128_make(REP), mmux_decimal128_make(IMP));
+    auto A = mmux_ctype_arg(Z);
+    assert(true == mmux_ctype_equal_relepsilon(ARG, A, EPS));
+    fprintf(stderr, "%s: Z=", __func__);
+    mmux_ctype_fprintf(stderr, Z);
+    fprintf(stderr, ", arg=");
+    mmux_ctype_fprintf(stderr, A);
+    fprintf(stderr, "\n");
+  }
+#endif
+
+}
+
+
+/** --------------------------------------------------------------------
  ** Let's go.
  ** ----------------------------------------------------------------- */
 
@@ -397,6 +570,7 @@ main (int argc MMUX_CC_TYPES_UNUSED, char const *const argv[] MMUX_CC_TYPES_UNUS
 
   if (1) {	test_complex_make_rectangular();	}
   if (1) {	test_complex_conjugate();		}
+  if (1) {	test_complex_arg();			}
 
   exit(EXIT_SUCCESS);
 }
